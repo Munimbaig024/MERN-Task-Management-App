4ed0f515b62488dfd95a8b8f4e11b1a67be62f62
Munimbaig024 <munimbaig12345@gmail.com>
2026-07-05 10:00:00 +0500

    feat: add Task model with User relation

---
+++ server/models/Task.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    title: {
+      type: String,
+      required: [true, 'Please add a task title'],
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+      default: '',
+    },
+    status: {
+      type: String,
+      enum: ['todo', 'in-progress', 'done'],
+      default: 'todo',
+    },
+    dueDate: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Task', taskSchema);
