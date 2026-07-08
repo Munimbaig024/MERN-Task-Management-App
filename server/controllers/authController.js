c2ed7aa16a8405d6fbfd0f89baa488f40c11993d
Munimbaig024 <munimbaig12345@gmail.com>
2026-07-08 10:00:00 +0500

    feat: add login endpoint

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -32,4 +32,31 @@ const registerUser = async (req, res) => {
   }
 };
 
-module.exports = { registerUser };
+// @desc    Authenticate user and get token
+// @route   POST /api/auth/login
+// @access  Public
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find user by email
+    const user = await User.findOne({ email });
+
+    // Check user exists and password matches
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    } else {
+      res.status(401).json({ message: 'Invalid email or password' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = { registerUser, loginUser };
+
